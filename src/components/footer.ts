6b782dff94f696dee5f5ab58c234443fb2ebc12b
Chris Marshall <thoughtpalettechris@gmail.com>
2024-12-03 16:53:23 -0600

    Updated copyright, email and yoe

--- src/components/footer.ts
+++ src/components/footer.ts
@@ -11,7 +11,7 @@ export class FooterComponent extends LitElement {
         return html`
         <footer class="flex flex-row p-4 text-black sticky top-[100vh] bg-slate-100 border-t border-gray-100">
                 <div class="container flex justify-end mx-auto md:flex-row text-xs text-slate-500">
-                    <p>MIT Licensed @ 2023 Christopher Marshall.</p>
+                    <p>MIT Licensed @ 2024 Christopher Marshall.</p>
                 </div>
         </footer>
         `
--- src/pages/contact-page.ts
+++ src/pages/contact-page.ts
@@ -26,8 +26,8 @@ export class ContactPage extends LitElement {
                     <article class="prose lg:prose-xl">
                         <h1 class="text-5xl prose font-bold">Experienced Web Engineer open to <span class="text-[#36d399]">Join Your Team</span></h1>
                         <p>Hello there! If you're seeking a skilled and enthusiastic engineer to join your team, I'm eager to connect and explore potential opportunities with your company.</p>
-                        <p>I bring <span class="text-[#6e0b75] font-bold">12+ years of experience</span> in <span class="text-[#f8860d] font-bold">front-end development</span> and have a passion for tackling challenging projects that drive innovation and excellence. Let's discuss how my expertise can contribute to your company's success.</p>
-                        <p>Feel free to send me an email at <a class="text-[#3abff8] underline hover:text-[#007ebd]" href="mailto:thoughtpalettechris+website@gmail.com">thoughtpalettechris at gmail dot com</a>. I'm looking forward to the possibility of working together and making a meaningful impact through engineering. Let's start a conversation that could shape an exciting future for both of us!</p>
+                        <p>I bring <span class="text-[#6e0b75] font-bold">14+ years of experience</span> in <span class="text-[#f8860d] font-bold">front-end development</span> and have a passion for tackling challenging projects that drive innovation and excellence. Let's discuss how my expertise can contribute to your company's success.</p>
+                        <p>Feel free to send me an email at <a class="text-[#3abff8] underline hover:text-[#007ebd]" href="mailto:chris@thoughtpalette.com">chris at thoughtpalette dot com</a>. I'm looking forward to the possibility of working together and making a meaningful impact through engineering. Let's start a conversation that could shape an exciting future for both of us!</p>
                     </article>
                 </div>
                 <footer-component></footer-component>
